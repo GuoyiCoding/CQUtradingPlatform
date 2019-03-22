1174d0498e033f2f16613e0a736fb5e816f3bb4a
Master <2732537769@qq.com>
2019-03-22 21:28:02 +0800

    Initial Commit

---
+++ pages/index/index.js
@@ -0,0 +1,55 @@
+//index.js
+//获取应用实例
+var app = getApp();
+
+Page({
+  data: {
+    goods: null,
+    num: 5, //默认首页初始化五条商品信息
+    classId: {
+      'phone': 1,
+      'book': 2,
+      'computer': 3,
+    }
+  },
+
+  onShow: function () {
+
+    var that = this
+    wx.request({
+      url: app.data.apiUrl + '/goods/' + that.data.isList + '/' + that.data.num,
+      success: function (res) {
+        that.setData({
+          goods: res.data.data
+        })
+      }
+    })
+  },
+  onLoad: function () {
+    wx.showToast({
+      title: '',
+      icon: 'loading',
+      duration: 1000
+    })
+  },
+  onReachBottom: function () {/*什么意思？？ */
+    var that = this
+    wx.showToast({
+      title: '',
+      icon: 'loading',
+      duration: 1000
+    })
+    that.setData({
+      num: that.data.num += 5
+    })
+    wx.request({
+      url: app.data.apiUrl + '/goods/' + that.data.isList + '/' + that.data.num,
+      success: function (res) {
+        that.setData({
+          goods: res.data.data
+        })
+      }
+    })
+  },
+})
+
---
+++ pages/new/new.js
@@ -0,0 +1,47 @@
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    img: null,
+    classId: null,
+    classification: null,
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+    var that = this
+    wx.request({
+      url: app.data.apiUrl + '/goods/class',
+      success: function (res) {
+        that.setData({
+          classification: res.data.data
+        })
+      }
+    })
+  },
+  bindPickerChange: function (e) {
+  var that = this
+  console.log('携带值为', e.detail.value)
+  that.setData({
+    classId: that.data.classification[e.detail.value].id
+  })
+  }
+})
